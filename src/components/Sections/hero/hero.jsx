e494f934f5a5d1adf4607d9eedad57ebda4a5900
avichal <avichalwebdev@gmail.com>
2022-10-18 14:24:36 +0530

    Created the hero section with the gradient effect

--- src/components/Sections/hero/hero.jsx
+++ src/components/Sections/hero/hero.jsx
@@ -1,13 +1,24 @@
+import Button from "../../UI/Button";
+import LinkUnderline from "../../UI/Link";
 import classes from "./hero.module.css";
+import heroImage from "../../../images/HeroImage.jpg";
+import Heading from "../../UI/Heading";
 
 export default function hero() {
    return (
-      <>
-         <h1 className={classes.heroText}>Flockfysh is so cool. blah blah blah blah</h1>
-         <div className={classes.heroImage}>
-            <img src="https://via.placeholder.com/1169x600?text=FlockFysh+Hero+Image" alt="Demo" />
+      <div className={classes.heroSectionDiv}>
+         <div className={classes.contentSection}>
+            <Heading
+               beforeSpan=""
+               span="Automatic "
+               afterSpan="dataset generation, minus the hassle."
+            />
+            <div className={classes.buttonsHolder}>
+               <Button text="Start Flockfyshing" to="/" />
+               <LinkUnderline to="/" text="Read the docs" />
+            </div>
          </div>
-         <div className={classes.sumContent}></div>
-      </>
+         <img src={heroImage} alt="HeroImage" className={classes.image} />
+      </div>
    );
 }
