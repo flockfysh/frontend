7106770893c61033f9baf196beb769b5bea94dfb
rjames187 <rory.james2021@gmail.com>
2022-11-09 20:16:11 -0600

    continue renaming of heading to header

--- src/components/UI/header/Header.jsx
+++ src/components/UI/header/Header.jsx
@@ -1,8 +1,8 @@
 import classes from "./Header.module.css";
 
-export default function Heading(props) {
+export default function Header(props) {
    return (
-      <h1 className={classes.heading}>
+      <h1 className={classes.header}>
          {props.beforeSpan}
          <span className={classes.span}>{props.span}</span>
          {props.afterSpan}
