25e342dbaaedd985a9229e890cd818088be2f6ce
rjames187 <rory.james2021@gmail.com>
2022-11-09 21:13:34 -0600

    implement mini profile of view datasets header

---
+++ src/components/UI/miniProfile/MiniProfile.jsx
@@ -0,0 +1,16 @@
+import classes from "./MiniProfile.module.css";
+
+export default function MiniProfile ({image, name, email}) {
+
+
+
+    return (
+        <div className={classes.mainDiv}>
+            <img className={classes.image} src={image} alt="" />
+            <div className={classes.textInfo}>
+                <div className={classes.name}>{name}</div>
+                <div className={classes.email}>{email}</div>
+            </div>
+        </div>
+    );
+}
