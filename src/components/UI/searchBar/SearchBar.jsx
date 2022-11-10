b36938d207271e0a8d8bec8717c073cb492e320b
rjames187 <rory.james2021@gmail.com>
2022-11-09 21:58:00 -0600

    create search bar component

---
+++ src/components/UI/searchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import classes from './SearchBar.module.css';
+import Icon from '../../../images/icons/search.svg';
+
+export default function SearchBar ({placeHolder, onChangeLambda}) {
+    return (
+        <div className={classes.container}>
+            <input type="text" placeholder={placeHolder} />
+            <img src={Icon} alt="" />
+        </div>
+    );
+}
