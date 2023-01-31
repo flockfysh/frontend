08476276784964dc3280f7a2d7abba9974536eac
Khanh <khanhtranngoccva@gmail.com>
2023-01-31 21:44:03 +0700

    feat: Partially fixed annotation UI

--- src/components/dashboard/eachDataset/sideBar/sideBar.tsx
+++ src/components/dashboard/eachDataset/sideBar/sideBar.tsx
@@ -1,8 +1,9 @@
 import {Link, useParams} from 'react-router-dom';
 import React, {PropsWithChildren} from "react";
 import classes from './sideBar.module.css';
-import {RxHamburgerMenu} from 'react-icons/rx';
+import {RxHamburgerMenu, RxArrowLeft} from 'react-icons/rx';
 import {ScreenContext} from "../../../../contexts/screenContext";
+import GradientLink from "../../../UI/gradientLink/gradientLink";
 
 const LINK_DESCRIPTIONS = [
     {
@@ -42,26 +43,35 @@ export default function SideBar(props: PropsWithChildren<{ name: string; page: s
     });
 
     return (
-        <div className={classes.navOverlay}>
+        <header className={classes.navOverlay}>
             <nav className={classes.primaryBar}>
-                <button className={classes.toggleMobileMenuButton} onClick={() => {
+                <Link className={classes.menuBarButton} to={"/dashboard"} title={"Back to dashboard"}>
+                    <RxArrowLeft/>
+                </Link>
+                <button className={classes.menuBarButton} onClick={function toggleMobileMenu() {
                     setMobileShown(!mobileShown);
-                }}>
+                }} title={"Menu"}>
                     <RxHamburgerMenu/>
                 </button>
-                <h1>{props.name} Dataset</h1>
+                <div className={classes.titleBar}>
+                    <Link className={`${classes.menuBarButton} ${classes.desktopBackButton}`} to={"/dashboard"} title={"Back to dashboard"}>
+                        <RxArrowLeft/>
+                    </Link>
+                    <h1 className={classes.datasetName}>{props.name} Dataset</h1>
+                </div>
                 <div className={classes.desktopEachDatasetLinks}>
-                    {links.map(link => {
+                    {links.map(function createLink(link) {
                         return <Link
                             to={link.to}
                             className={
-                                `${props.page === link.name ? classes.linkColored : ""} ${classes.link}`
+                                `${props.page === link.name ? classes.linkColored : ""} ${classes.link} ${classes.desktopLink}`
                             }
                             key={link.to}>
                             {link.children}
                         </Link>;
                     })}
                 </div>
+
             </nav>
             <div className={classes.mainContainer}>
                 <div className={`${classes.mobileSidebarOverlay} ${mobileShownClassName}`} onClick={(e) => {
@@ -80,6 +90,6 @@ export default function SideBar(props: PropsWithChildren<{ name: string; page: s
                 </div>
                 {props.children}
             </div>
-        </div>
+        </header>
     );
 }
--- src/pages/annotate/annotate.tsx
+++ src/pages/annotate/annotate.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 import GradientLink from '../../components/UI/gradientLink/gradientLink';
 import Label from '../../components/dashboard/annotate/label/label';
@@ -6,102 +6,81 @@ import Wrapper from '../../components/dashboard/annotate/wrapper/wrapper';
 import Loading from '../../components/loading/loading';
 
 import classes from './annotate.module.css';
+import {RxArrowLeft, RxArrowRight, RxPlus} from 'react-icons/rx';
 
 export default function Annotate() {
-	const [images, updateImages] = useState([] as Image[]);
-	
-	const [imageIndex, updateImageIndex] = useState(0);
-	const [isLoading, updateLoading] = useState(true);
+    const [images, updateImages] = useState([] as Image[]);
 
-	useEffect(() => {
-		updateImages(
-			[
-				{
-					url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmzsi4JX8QgMg_J0-xUHxeJ9Ot_2zVfoh2Gw&usqp=CAU',
-					name: 'dog'
-				}
-			]
-		);
-	}, []);
+    const [imageIndex, updateImageIndex] = useState(0);
+    const [isLoading, updateLoading] = useState(true);
 
-	useEffect(()=>{
-		updateLoading(false);
-	}, [images]);
-	const rect = {
-		x: 150,
-    	y: 150,
-    	width: 100,
-    	height: 100
-	};
+    useEffect(() => {
+        updateImages(
+            [
+                {
+                    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmzsi4JX8QgMg_J0-xUHxeJ9Ot_2zVfoh2Gw&usqp=CAU',
+                    name: 'dog'
+                }
+            ]
+        );
+    }, []);
 
-	if(isLoading) return <Loading />;
+    useEffect(() => {
+        updateLoading(false);
+    }, [images]);
+    const rect = {
+        x: 150,
+        y: 150,
+        width: 100,
+        height: 100
+    };
+
+    if (isLoading) return <Loading/>;
 
     return (
-        <div className={ classes.annotateContainer }>
-			<h1>Picture - { imageIndex + 1 }/50</h1>
+        <div className={classes.annotateContainer}>
+            <div className={classes.headingContainer}>
+                <h1 className={classes.heading}>Picture - {imageIndex + 1}/50</h1>
+            </div>
+            <div className={classes.submitButtonContainer}>
+                <GradientLink to="/" text="Initiate training" gradientDirection="rightToLeft"
+                              className={classes.initiateTrainingButton}/>
+            </div>
+            <div className={classes.leftContainer}>
+                <div
+                    className={classes.wrapperDiv}
+                    style={
+                        {
+                            backgroundImage: "url(" + images[imageIndex].url + ")"
+                        }
+                    }
+                >
+                </div>
 
-			<div className={ classes.contentContainer }>
-				<div className={ classes.leftContainer }>
-					<div 
-						className={ classes.wrapperDiv } 
-						style={
-							{
-								backgroundImage: "url("+images[imageIndex].url+")"
-							}
-						}
-					>
-						<Wrapper rect={ rect } imgWidth={ 635 } imgHeight={ 423 }/>
-					</div>
 
-					<div className={ classes.labelsContainer }>
-						<Label text="Dog" />
-						<Label text="Playing" />
-						<Label text="Golden Spaniel" />
+            </div>
 
-						<button className={ classes.addLabelButton }>+</button>
-					</div>
-				</div>
+            <div className={classes.rightContainer}>
+                <div className={classes.box}/>
+            </div>
+            <div className={classes.labelContainer}>
+                <div className={classes.labelList}>
+                    <Label text="Dog"/>
+                    <Label text="Playing"/>
+                    <Label text="Golden Spaniel"/>
+                    <Label text="Golden Spaniel"/>
+                </div>
+                <button className={classes.addLabelButton}><RxPlus/></button>
+            </div>
+            <div className={classes.switchImageContainer}>
+                <button className={classes.switchImageButton}>
+                    <RxArrowLeft className={classes.switchImageIcon}/>
+                </button>
 
-				<div className={ classes.rightContainer }>
-					<div className={ classes.submitButtonContainer }>
-						<GradientLink to="/" text="Submit" gradientDirection="rightToLeft" />
-					</div>
-
-					<div className={ classes.box } />
-
-					<div className={ classes.switchImageContainer }>
-						<button className={ classes.switchImageButton }> 
-							<svg
-								className={ classes.svg + ' ' + classes.leftSvg }
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-								/>
-							</svg>
-						</button>
-		
-						<button className={ classes.switchImageButton }> 
-							<svg
-								className={ classes.svg }
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-								/>
-							</svg>
-						</button>
-					</div>
-				</div>
-			</div>	
+                <button className={classes.switchImageButton}>
+                    <RxArrowRight className={classes.switchImageIcon}/>
+                </button>
+            </div>
         </div>
     );
 }
