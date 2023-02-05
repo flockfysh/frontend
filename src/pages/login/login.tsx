3e91954835b21a60f4e8fe7c5a2aeb51d8d652f6
Khanh <khanhtranngoccva@gmail.com>
2023-02-05 19:27:17 +0700

    fix: The login UI will not be shown if the user already has logged in

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,5 @@
 import {useState, useRef, useContext} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import {Link, Navigate, useNavigate} from 'react-router-dom';
 
 import {serverURL} from '../../settings';
 
@@ -8,8 +8,12 @@ import {UserContext} from '../../contexts/userContext';
 import classes from './login.module.css';
 
 export default function LoginForm(props: { type: string }) {
-    const navigate = useNavigate();
     const curPopup = useRef<Window | null>(null);
+    const {isLoggedIn} = useContext(UserContext);
+
+    if (isLoggedIn) {
+        return <Navigate to="/dashboard" replace={true} />;
+    }
 
     // defaulting to true to account for browser auto-filling
     const [emailIsValid, setEmailIsValid] = useState(true);
