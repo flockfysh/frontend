0f9bd870aa0c52fe4df2cb7c8786560e7fbe1217
Khanh Tran <khanhtranngoccva@gmail.com>
2023-02-21 16:40:35 +0700

    feat: Delete dataset API call

--- src/components/dashboard/viewDatasets/datasetCard/datasetCard.tsx
+++ src/components/dashboard/viewDatasets/datasetCard/datasetCard.tsx
@@ -4,6 +4,7 @@ import LinkUnderline from '../../../UI/linkUnderline/linkUnderline';
 
 import Button from '../../../UI/button/button';
 import classes from './datasetCard.module.css';
+import api from '../../../../helpers/api';
 
 export default function DatasetCard(props: { dataset: PartialDataset }) {
     return (
@@ -11,23 +12,25 @@ export default function DatasetCard(props: { dataset: PartialDataset }) {
             <div className={ classes.cardHeader }>
 
                 <h2 className={ classes.datasetName }>
-                    { props.dataset.name }
+                    {props.dataset.name}
                 </h2>
-                <Button className={ classes.deleteButton }>
+                <Button className={ classes.deleteButton } onClick={ async () => {
+                    await api.delete(`/api/datasets/${props.dataset.id}`);
+                } }>
                     <FaTrash className={ classes.trashIcon }></FaTrash>
                 </Button>
             </div>
 
-            <p className={ classes.overview }>{ props.dataset.description }</p>
+            <p className={ classes.overview }>{props.dataset.description}</p>
 
             <div className={ classes.cardBottom }>
-                <LinkUnderline 
+                <LinkUnderline
                     className={ classes.viewDatasetLink }
-                    to={ `/dashboard/${ props.dataset.id }/overview/` }
+                    to={ `/dashboard/${props.dataset.id}/overview/` }
                     text="View dataset"
                 />
 
-                <span>{ props.dataset.itemCount } Images</span>
+                <span>{props.dataset.itemCount} Images</span>
             </div>
         </li>
     );
