e9eb5a72353145032e8bc4c0e6a0f14cf08692af
Khanh Tran <khanhtranngoccva@gmail.com>
2023-02-26 14:18:28 +0700

    fix: Completed datasets cannot access the training page

--- src/components/dashboard/eachDataset/train/train.tsx
+++ src/components/dashboard/eachDataset/train/train.tsx
@@ -8,7 +8,7 @@ import { LABEL_COLORS } from '../../../../settings';
 import api from '../../../../helpers/api';
 import { AxiosError } from 'axios';
 import { ErrorContext } from '../../../../contexts/errorContext';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 interface InitiateTraningRequest {
     class_search_queries: Record<string, string[]>;
@@ -32,6 +32,11 @@ export default function Train(props: { dataset: Dataset }) {
 
     const navigate = useNavigate();
     const { throwError } = React.useContext(ErrorContext);
+
+    if (props.dataset.state === 'completed') {
+        return <Navigate to={ `/dashboard/${props.dataset.id}/dataset-images` }/>;
+    }
+
     async function initiateSubmission(e: React.FormEvent<HTMLFormElement>) {
         try {
             e.preventDefault();
@@ -59,50 +64,58 @@ export default function Train(props: { dataset: Dataset }) {
             }
             navigate('../annotate');
         }
-        catch (error) {
+ catch (error) {
             if (error instanceof AxiosError) {
                 throwError(error.response?.data.error.message, 'Training error');
             }
         }
     }
 
+    const buttonLabel: Record<string, React.ReactNode> = {
+        'untrained': <span>Initiate training</span>,
+        'feedback': <span>Continue training</span>,
+        'completed': null,
+    };
+
     return (
         <div className={ classes.container }>
             <form className={ classes.contentContainer } onSubmit={ initiateSubmission }>
                 <div className={ classes.titleBar }>
                     <h1>Dataset training</h1>
-                    <Button type={ 'submit' } disabled={ props.dataset.state !== 'untrained' }className={ classes.utilityButton } gradient={ true }>
-                        <span>Initiate training</span>
+                    <Button type={ 'submit' } className={ classes.utilityButton } gradient={ true }>
+                        {buttonLabel[props.dataset.state]}
                         <RxArrowRight className={ classes.icon }></RxArrowRight>
                     </Button>
                 </div>
-                <div className={ classes.formInputs }>
-                    <div className={ classes.labelledInputContainer }>
-                        <span className={ classes.labelledInputContainer__label }>Search Queries</span>
-                        <ul className={ trainingClasses.trainingQueriesInputs }>
-                            {props.dataset.classes.map(function generateSearchQueryInput(classString, index) {
-                                const id = React.useId();
-                                return (
-                                    <React.Fragment key={ index }>
-                                        <TrainingLabels htmlFor={ id }
-                                                        labelColor={ LABEL_COLORS[index] }>{classString}</TrainingLabels>
-                                        <CustomCreatableSelect name={ `searchQuery_${index}` } instanceId={ id }
-                                                               isMulti={ true }
-                                                               className={ trainingClasses.querySelect }></CustomCreatableSelect>
-                                    </React.Fragment>
-                                );
-                            })}
-                        </ul>
+                {props.dataset.state === 'untrained' ? (
+                    <div className={ classes.formInputs }>
+                        <div className={ classes.labelledInputContainer }>
+                            <span className={ classes.labelledInputContainer__label }>Search Queries</span>
+                            <ul className={ trainingClasses.trainingQueriesInputs }>
+                                {props.dataset.classes.map(function generateSearchQueryInput(classString, index) {
+                                    const id = React.useId();
+                                    return (
+                                        <React.Fragment key={ index }>
+                                            <TrainingLabels htmlFor={ id }
+                                                            labelColor={ LABEL_COLORS[index] }>{classString}</TrainingLabels>
+                                            <CustomCreatableSelect name={ `searchQuery_${index}` } instanceId={ id }
+                                                                   isMulti={ true }
+                                                                   className={ trainingClasses.querySelect }></CustomCreatableSelect>
+                                        </React.Fragment>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+
+                        <label className={ classes.labelledInputContainer }>
+                            <span className={ classes.labelledInputContainer__label }>Output Quantity</span>
+                            <input type="number" id={ 'desired_data' } name={ 'desired_data' } min={ 1 } max={ 10000 }
+                                   defaultValue={ 30 }
+                                   className={ classes.outputQuantity }/>
+                        </label>
+
                     </div>
-
-                    <label className={ classes.labelledInputContainer }>
-                        <span className={ classes.labelledInputContainer__label }>Output Quantity</span>
-                        <input type="number" id={ 'desired_data' } name={ 'desired_data' } min={ 1 } max={ 10000 }
-                               defaultValue={ 30 }
-                               className={ classes.outputQuantity }/>
-                    </label>
-
-                </div>
+                ) : <></>}
             </form>
         </div>
     );
