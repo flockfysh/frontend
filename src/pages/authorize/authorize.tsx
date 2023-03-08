685ca3b91056b851bebd50cd318886a2ed7851fd
Khanh Tran <khanhtranngoccva@gmail.com>
2023-03-08 22:56:31 +0700

    feat: OAuth device authorization flow UI completed

--- src/pages/authorize/authorize.tsx
+++ src/pages/authorize/authorize.tsx
@@ -1,18 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import type { IconBase } from 'react-icons';
 import { RxEyeOpen, RxPencil1 } from 'react-icons/rx';
+import { FaMoneyBillAlt, FaCloud } from 'react-icons/fa';
+import { IoPerson } from 'react-icons/io5';
 import Loading from '../../components/loading/loading';
 import api from '../../helpers/api';
 import classes from './authorize.module.css';
+import Button from '../../components/UI/button/button';
+import GradientLink from '../../components/UI/gradientLink/gradientLink';
 
+type AuthorizationFeedback = 'reject' | 'approve';
 const PERMISSION_MAPPING: Record<string, { description: string, Icon: typeof IconBase }> = {
     'write_dataset': {
-        description: 'Can write, edit and delete datasets',
+        description: 'Create, provide feedback and delete datasets',
         Icon: RxPencil1,
     },
     'read_dataset': {
-        description: 'Can read datasets',
+        description: 'Read datasets and access images',
         Icon: RxEyeOpen,
+    },
+    'share_dataset': {
+        description: 'Share your datasets to other organizations',
+        Icon: FaCloud,
+    },
+    'manage_account': {
+        description: 'Manage your account',
+        Icon: IoPerson,
     },
 };
 
@@ -24,11 +37,22 @@ interface AuthorizationInstance {
     };
 }
 
+function PermissionItem(props: { description: string, Icon: typeof IconBase }) {
+    return (
+        <li className={ classes.permissionItem }>
+            <props.Icon className={ classes.permissionIcon }></props.Icon>
+            <p>
+                {props.description}
+            </p>
+        </li>
+    );
+}
+
 export default function Authorize() {
     const code = new URL(window.location.href).searchParams.get('code');
 
     const [authorizationInstance, setAuthorizationInstance] = React.useState<AuthorizationInstance | null>(null);
-    const [message, setMessage] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         (async function authorizeLogin() {
@@ -39,27 +63,76 @@ export default function Authorize() {
                         user_code: code,
                     },
                 })).data.data;
-                console.log(instanceInfo);
                 setAuthorizationInstance(instanceInfo);
             }
  catch (e) {
-                setMessage('Error');
+                setError('This authorization code is either invalid or expired.');
             }
 
         })();
     }, []);
 
+    async function authorize(mode: AuthorizationFeedback) {
+        const urls: Record<AuthorizationFeedback, string> = {
+            'approve': '/api/auth/oauth/deviceAuthorization/accept',
+            'reject': '/api/auth/oauth/deviceAuthorization/reject',
+        };
+
+        await api.post(urls[mode], {}, {
+            params: {
+                // eslint-disable-next-line camelcase
+                user_code: code,
+            },
+        });
+
+        window.close();
+    }
+
+    if (error) {
+        return (
+            <div className={ classes.authContainer }>
+                <div className={ classes.authContainerInner }>
+                    <h2 className={ classes.errorHeading }>Authorization error</h2>
+                    <p>{error}</p>
+                    <GradientLink to={ '/dashboard' }>Go to dashboard</GradientLink>
+                </div>
+            </div>
+        );
+    }
+
     if (!authorizationInstance) return <Loading/>;
 
     return (
         <div className={ classes.authContainer }>
-            {
-                !message ? (
-                    <h1>Success!</h1>
-                ) : (
-                    <h1>{message}</h1>
-                )
-            }
+            <form className={ classes.authContainerInner }>
+                <div className={ classes.appDetails }>
+                    <div className={ classes.appLogo }></div>
+                    <h2 className={ classes.appName }>{authorizationInstance.client.name}</h2>
+                    <p>wants to:</p>
+                </div>
+                <ul className={ classes.permissionList }>
+                    {authorizationInstance.scope.map(function generatePermissionItem(scope, index) {
+                        if (PERMISSION_MAPPING[scope]) {
+                            const permObject = PERMISSION_MAPPING[scope];
+                            return (
+                                <PermissionItem description={ permObject.description } Icon={ permObject.Icon }
+                                                key={ index }/>
+                            );
+                        }
+                        return <></>;
+                    })}
+                </ul>
+                <div className={ classes.authorizationButtons }>
+                    <Button className={ classes.rejectAuthorizationButton } onClick={ async e => {
+                        e.preventDefault();
+                        await authorize('reject');
+                    } }>Reject</Button>
+                    <Button gradient={ true } onClick={ async e => {
+                        e.preventDefault();
+                        await authorize('approve');
+                    } }>Approve</Button>
+                </div>
+            </form>
         </div>
     );
 }
