e35122f23fb814af8d83eb801b9d9789fd6a6ca2
Khanh Tran <khanhtranngoccva@gmail.com>
2023-03-08 15:32:13 +0700

    feat: Fetch device authorization instance from backend

--- src/pages/authorize/authorize.tsx
+++ src/pages/authorize/authorize.tsx
@@ -1,27 +1,55 @@
-import { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import type { IconBase } from 'react-icons';
+import { RxEyeOpen, RxPencil1 } from 'react-icons/rx';
 import Loading from '../../components/loading/loading';
-
+import api from '../../helpers/api';
 import classes from './authorize.module.css';
 
+const PERMISSION_MAPPING: Record<string, { description: string, Icon: typeof IconBase }> = {
+    'write_dataset': {
+        description: 'Can write, edit and delete datasets',
+        Icon: RxPencil1,
+    },
+    'read_dataset': {
+        description: 'Can read datasets',
+        Icon: RxEyeOpen,
+    },
+};
+
+interface AuthorizationInstance {
+    scope: string[];
+    client: {
+        name: string;
+        website: string;
+    };
+}
+
 export default function Authorize() {
-    const [isLoading, updateLoading] = useState(true);
+    const code = new URL(window.location.href).searchParams.get('code');
+
+    const [authorizationInstance, setAuthorizationInstance] = React.useState<AuthorizationInstance | null>(null);
     const [message, setMessage] = useState<string | null>(null);
 
     useEffect(() => {
         (async function authorizeLogin() {
             try {
-
+                const instanceInfo = (await api.get('/api/auth/oauth/deviceAuthorization/public', {
+                    params: {
+                        // eslint-disable-next-line camelcase
+                        user_code: code,
+                    },
+                })).data.data;
+                console.log(instanceInfo);
+                setAuthorizationInstance(instanceInfo);
             }
-            catch(e) {
+ catch (e) {
                 setMessage('Error');
             }
 
-            updateLoading(false);
         })();
     }, []);
 
-    if (isLoading) return <Loading/>;
+    if (!authorizationInstance) return <Loading/>;
 
     return (
         <div className={ classes.authContainer }>
@@ -29,7 +57,7 @@ export default function Authorize() {
                 !message ? (
                     <h1>Success!</h1>
                 ) : (
-                    <h1>{ message }</h1>
+                    <h1>{message}</h1>
                 )
             }
         </div>
