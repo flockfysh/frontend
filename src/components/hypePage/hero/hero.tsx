060703d906fca0fef38aedb797b64c8de769eeeb
Pedro Carreño <pkcarrenodev@gmail.com>
2023-03-22 23:26:26 -0400

    feat: feedback on successful email

--- src/components/hypePage/hero/hero.tsx
+++ src/components/hypePage/hero/hero.tsx
@@ -8,33 +8,42 @@ import api from '../../../helpers/api';
 export default function Hero() {
     const [accessRequestSuccess, setAccessRequestSuccess] = React.useState(false);
     const [errorMessage, updateErrorMessage] = React.useState('');
+    const [successMessage, updateSuccesMessage] = React.useState('');
     const waitlistFormRef = React.useRef<HTMLFormElement | null>(null);
 
     const onEmailChange = React.useCallback(() => {
-        if (errorMessage) {
+        if (errorMessage !== '') {
             updateErrorMessage('');
+        }
+        if (successMessage !== '') {
+            updateSuccesMessage('');
         }
     }, [errorMessage]);
 
     async function addUserToWaitlist(e: React.MouseEvent) {
         e.preventDefault();
-        if (waitlistFormRef.current) {
-            const waitlistForm = waitlistFormRef.current;
-            const fd = new FormData(waitlistForm);
-            const email = fd.get('email') as string;
-            if (!email) {
-                updateErrorMessage('Please enter an email.');
-                return;
+        try {
+            if (waitlistFormRef.current) {
+              const waitlistForm = waitlistFormRef.current;
+              const fd = new FormData(waitlistForm);
+              const email = fd.get('email') as string;
+              if (!email) {
+                  updateErrorMessage('Please enter an email.');
+                  return;
+              }
+              else if (!validateEmail(email)) {
+                  updateErrorMessage('Please enter a valid email.');
+                  return;
+              }
+              await api.post('/api/auth/waitlist', {
+                  email,
+              });
+              updateSuccesMessage('You are on the waiting list');
+              setAccessRequestSuccess(true);
             }
-            else if (!validateEmail(email)) {
-                updateErrorMessage('Please enter a valid email.');
-
-                return;
-            }
-            await api.post('/api/auth/waitlist', {
-                email,
-            });
-            setAccessRequestSuccess(true);
+        }
+        catch (Error) {
+            throw Error;
         }
     }
 
@@ -65,12 +74,17 @@ export default function Hero() {
                     className={ classes.extraInfo }>Designed for developers, researchers and those who dare to dream.</span>
             </div>
 
-            <form className={ classes.inputEmail } ref={ waitlistFormRef }>
+            <form className={ `${classes.inputEmail} ${successMessage !== '' ? classes.inputSuccessEmail : ''}` } ref={ waitlistFormRef }>
                 <div>
-                    <input onChange={ onEmailChange } type="email" name="email" placeholder="Your email here"/>
+                    <input onChange={ onEmailChange } 
+                           type="email" 
+                           name="email" 
+                           placeholder="Your email here"/>
                     <label>{errorMessage}</label>
+                    <span>{successMessage}</span>
                 </div>
                 <button type="submit"
+                        disabled={ successMessage !== '' }
                         onClick={ addUserToWaitlist }
                         className={ `${classes.submitButton} ${accessRequestSuccess ? classes.submitSuccess : ''}` }>{accessRequestSuccess ? 'Access request sent!' : 'Request access'}
                 </button>
