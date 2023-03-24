146f9266a2f4b8e3c8af213d5e018b10d7528312
Khanh Tran <khanhtranngoccva@gmail.com>
2023-03-24 15:24:54 +0700

    feat: Background scroll with visible x-overflow and hidden y-overflow

--- src/components/hypePage/background/background.tsx
+++ src/components/hypePage/background/background.tsx
@@ -1,34 +1,41 @@
 import classes from './background.module.css';
 
 export default function Background() {
+    const contents = (
+        <div className={ classes.backgroundWrapper }>
+            <div className={ classes.gradient1 }>
+                <div className={ classes.rectangle1_1 }>
+                    <div className={ classes.shape1 }>
+                        <div className={ classes.vector1_1 }/>
+                        <div className={ classes.vector1_2 }/>
+                        <div className={ classes.vector1_3 }/>
+                        <div className={ classes.vector1_4 }/>
+                        <div className={ classes.vector1_5 }/>
+                    </div>
+                </div>
+                <div className={ classes.rectangle1_2 }/>
+            </div>
+
+            <div className={ classes.gradient2 }>
+                <div className={ classes.rectangle2_1 }>
+                    <div className={ classes.shape2 }>
+                        <div className={ classes.vector2_1 }/>
+                        <div className={ classes.vector2_2 }/>
+                        <div className={ classes.vector2_3 }/>
+                        <div className={ classes.vector2_4 }/>
+                        <div className={ classes.vector2_5 }/>
+                    </div>
+                </div>
+                <div className={ classes.rectangle2_2 }/>
+            </div>
+        </div>
+    );
 
     return (
         <div className={ classes.backgroundWrapper2 }>
-            <div className={ classes.backgroundWrapper }>
-                <div className={ classes.gradient1 }>
-                    <div className={ classes.rectangle1_1 }>
-                        <div className={ classes.shape1 }>
-                            <div className={ classes.vector1_1 }/>
-                            <div className={ classes.vector1_2 }/>
-                            <div className={ classes.vector1_3 }/>
-                            <div className={ classes.vector1_4 }/>
-                            <div className={ classes.vector1_5 }/>
-                        </div>
-                    </div>
-                    <div className={ classes.rectangle1_2 }/>
-                </div>
-
-                <div className={ classes.gradient2 }>
-                    <div className={ classes.rectangle2_1 }>
-                        <div className={ classes.shape2 }>
-                            <div className={ classes.vector2_1 }/>
-                            <div className={ classes.vector2_2 }/>
-                            <div className={ classes.vector2_3 }/>
-                            <div className={ classes.vector2_4 }/>
-                            <div className={ classes.vector2_5 }/>
-                        </div>
-                    </div>
-                    <div className={ classes.rectangle2_2 }/>
+            <div className={ classes.backgroundWrapper3 }>
+                <div className={ classes.backgroundWrapper4 }>
+                    {contents}
                 </div>
             </div>
         </div>
