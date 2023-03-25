2a8585802a2aface69d99861df4e77dddea26314
Khanh Tran <khanhtranngoccva@gmail.com>
2023-03-25 13:30:31 +0700

    feat: Progress bar

--- src/components/dashboard/eachDataset/progressScreen/progressScreen.tsx
+++ src/components/dashboard/eachDataset/progressScreen/progressScreen.tsx
@@ -1,4 +1,5 @@
 import classes from '../common.module.css';
+import progressScreenClasses from './progressScreen.module.css';
 import dayjs from 'dayjs';
 import Duration from 'dayjs/plugin/duration';
 import RelativeTime from 'dayjs/plugin/relativeTime';
@@ -12,6 +13,21 @@ export interface ProgressScreenProps {
 
 dayjs.extend(Duration);
 dayjs.extend(RelativeTime);
+
+
+function ProgressBar(props: { current: number, total: number }) {
+    return (
+        <>
+            <progress max={ props.total } value={ props.current }
+                      className={ progressScreenClasses.progressBarAccessibility }></progress>
+            <div className={ progressScreenClasses.progressBarOuter }>
+                <div className={ progressScreenClasses.progressBarInner } style={ {
+                    width: `${props.current / props.total * 100}%`,
+                } }></div>
+            </div>
+        </>
+    );
+}
 
 export default function ProgressScreen(props: ProgressScreenProps) {
     let progressBarETA;
@@ -29,8 +45,7 @@ export default function ProgressScreen(props: ProgressScreenProps) {
         <>
             <div className={ classes.container }>
                 <div className={ classes.progressContainer }>
-                    <progress max={ props.total } value={ props.current }
-                              className={ classes.progressBar }></progress>
+                    <ProgressBar current={ props.current } total={ props.total }></ProgressBar>
                     <p>{props.description}</p>
                     {progressBarETA}
                 </div>
