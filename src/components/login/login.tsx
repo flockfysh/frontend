16718ff3cab65eed30c55adac543f28bac0cda61
Ray Cai Flanagan <hjkareus@gmail.com>
2023-05-12 16:07:38 -0400

    more changes to login

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,20 +1,46 @@
 import { useState } from 'react';
-import { GrClose } from 'react-icons/gr';
+import Link from 'next/link';
+import { ReactSVG } from 'react-svg';
+
+import xMark from '@/icons/xmark.svg';
+import google from '@/icons/providers/google.svg';
+import github from '@/icons/providers/github.svg';
+
+import ActionPopup from '../modals/actionPopup/actionPopup';
 
 import classes from './login.module.css';
+
+function Separator() {
+    return (
+        <div className={ classes.separator }>
+            <span>OR</span>
+        </div>
+    );
+}
 
 export default function Login() {
     const [isLogin, updateIsLogin] = useState(true);
 
     return (
-        <div className={ classes.overlay }>
-            <div className={ classes.container }>
-                <div className={ classes.header }>
-                    <GrClose />
+        <ActionPopup blurBg={ true }>
+            <div className={ classes.header }>
+                <ReactSVG src={ xMark } />
 
-                    <h1>{ isLogin ? 'Sign In' : 'Sign Up'}</h1>
-                </div>
+                <h1>{ isLogin ? 'Sign In' : 'Sign Up' }</h1>
             </div>
-        </div>
+
+            <div className={ classes.oAuthContainer }>
+                <button className={ classes.oAuthBtn }>
+                    <ReactSVG src={ google } /> { isLogin ? 'Sign In' : 'Sign Up' } with Google
+                </button>
+
+                <button className={ classes.oAuthBtn }>
+                    <ReactSVG src={ github } /> { isLogin ? 'Sign In' : 'Sign Up' } with GitHub
+                </button>
+            </div>
+
+            <Separator />
+
+        </ActionPopup>
     );
 }
--- src/components/modals/actionPopup/actionPopup.tsx
+++ src/components/modals/actionPopup/actionPopup.tsx
@@ -1,3 +1,17 @@
-export default function ActionPopup() {
+import { PropsWithChildren } from 'react';
 
+import classes from './actionPopup.module.css';
+
+interface ActionPopupProps extends PropsWithChildren {
+    blurBg: boolean;
+}
+
+export default function ActionPopup(props: ActionPopupProps) {
+    return (
+        <div className={ classes.overlay + (props.blurBg ? classes.blur : '') }>
+            <div className={ classes.container }>
+                { props.children }
+            </div>
+        </div>
+    );
 }
