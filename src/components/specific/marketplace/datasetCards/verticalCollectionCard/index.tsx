047a187f7f7fb3d0149009f61ff492acfcf89e7e
avichal <avichalwebdev@gmail.com>
2023-06-20 17:00:29 +0530

    dataset section+header

--- src/components/specific/marketplace/datasetCards/verticalCollectionCard/index.tsx
+++ src/components/specific/marketplace/datasetCards/verticalCollectionCard/index.tsx
@@ -25,7 +25,7 @@ export default function VerticalCollectionCard(props: VerticalCollectionCardProp
                     <h1>{ props.name }</h1>
                     
                     <div className={ classes.profileCardContainer }>
-                        <ProfileCard username={ props.owner } profilePicture="https://s3-alpha-sig.figma.com/img/e182/5c4b/15ad61311df48a8ac412367e9229eff2?Expires=1686528000&Signature=JnhQXDAcac3h42gK90KMVVwO3bNqQfopveEVire8BdnqE3ugdcaEbZZNPqTyUOaSDPFVuO4GEZxGJFNBIMnJ1~WmLO-auhZ8QQBNJwRjQLvBPrY2s0agLL0P7iss9bxLPqs-MwXhdsi0IWx-x8KLBd8luJfyeQ0JTEnR3xIzZDZGTKD6MrhemD7YBK9wK7Kgs7viopmRSCEFo3ETxSuy-kANz7AXgEigfoh-RntFQ8-nEJUc8Ne9thw5nA~n3EX4LaQzb0NdDShTPFh5CPGfjMaVKHxSeSxwXWpRMdb-FbuNe3hRkASbx3riRJ6g5JwTiyESxYql8QAGdQ2QNejYnA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+                        <ProfileCard username={ props.owner } profilePicture="https://images.unsplash.com/photo-1531720414817-c690e57c5637?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1333&q=80" />
                     </div>
                 </div>
 
