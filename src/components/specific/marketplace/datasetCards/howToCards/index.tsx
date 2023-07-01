2a18b6e62a6df544880b5a0010902aabba31765b
Khanh Tran <khanhtranngoccva@gmail.com>
2023-07-01 23:09:35 +0700

    fix: Lint fixes

--- src/components/specific/marketplace/datasetCards/howToCards/index.tsx
+++ src/components/specific/marketplace/datasetCards/howToCards/index.tsx
@@ -10,33 +10,36 @@ export default function HowToCards() {
     return (
         <div className={ classes.container }>
             <div className={ classes.card }>
-                <ReactSVG className={ classes.headerIcon } src={ user.src } />
-            
+                <ReactSVG className={ classes.headerIcon } src={ user.src }/>
+
                 <div className={ classes.cardBase }>
                     <h1>Buy and Sell AI</h1>
 
-                    <p> From buy, renting, to selling AI models and datasets on your terms, easily earn recurring revenue while advancing AI. </p>
+                    <p> From buy, renting, to selling AI models and datasets on your terms, easily earn recurring
+                        revenue while advancing AI. </p>
                 </div>
             </div>
 
             <div className={ classes.card }>
-                <ReactSVG className={ classes.headerIcon + ' ' + classes.noBorder } src={ upload.src } />
-            
+                <ReactSVG className={ classes.headerIcon + ' ' + classes.noBorder } src={ upload.src }/>
+
                 <div className={ classes.cardBase }>
                     <h1> Ethically build and QA large datasets</h1>
 
-                    <p> Directly interact with those who provide the data and obtain anonymized, risk free data. Seamless run workflows that ensure your data is top notch, while easily finding QA experts. </p>
+                    <p> Directly interact with those who provide the data and obtain anonymized, risk free data.
+                        Seamless run workflows that ensure your data is top notch, while easily finding QA experts. </p>
                 </div>
             </div>
 
 
             <div className={ classes.card }>
-                <ReactSVG className={ classes.headerIcon + ' ' + classes.noBorder } src={ users.src } />
-            
+                <ReactSVG className={ classes.headerIcon + ' ' + classes.noBorder } src={ users.src }/>
+
                 <div className={ classes.cardBase }>
                     <h1>Collaborate</h1>
 
-                    <p> Work together to build some awesome AI services and use flockfysh's platform as a stepping stone to commercialize your venture or directly support the open source community.</p>
+                    <p> Work together to build some awesome AI services and use flockfysh&apos;s platform as a stepping
+                        stone to commercialize your venture or directly support the open source community.</p>
                 </div>
             </div>
         </div>
