996aa1f4f13e8539c04b6f9bf3211f5447395da9
Ray Cai Flanagan <hjkareus@gmail.com>
2023-07-01 17:50:30 -0400

    Update logout.tsx

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import api from '@/helpers/api';
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+
+import api from '@/helpers/api';
 
 export default function Logout() {
     const router = useRouter();
 
-    React.useEffect(() => {
+    useEffect(() => {
         async function logout() {
             await api.get('/api/auth/logout');
             router.replace('/login').then();
         }
 
         logout().then();
-    }, []);
+    }, [router]);
 }
