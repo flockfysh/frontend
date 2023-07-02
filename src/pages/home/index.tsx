47d13fac8cf9fd7edfa7294dc4b483b9a6f98602
Pedro Carreño <pkcarrenodev@gmail.com>
2023-07-01 23:04:09 -0400

    fix: background images

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,20 @@
+import Background from '@/components/hypePage/background/background';
 import Features from '../../components/hypePage/features/features';
 import Hero from '../../components/hypePage/hero/hero';
 import Roadmap from '../../components/hypePage/roadmap';
+import classes from './styles.module.css';
 
 export default function HypePage() {
     return (
-        <>
+        <div className={ classes.mainHypepageContainer }>
+            <div className={ classes.mainHypepageWrapper }>
             <Hero />
             <Features />
             
             <Roadmap />
 
-            {/*<Background />*/}
-        </>
+            <Background />
+            </div>
+        </div>
     );
 }
