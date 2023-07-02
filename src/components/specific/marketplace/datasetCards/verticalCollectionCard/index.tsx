d1197f4105c8627c7d59c07c44fd8e0171efdb34
avichal <avichalwebdev@gmail.com>
2023-07-02 23:51:29 +0530

    gradients

--- src/components/specific/marketplace/datasetCards/verticalCollectionCard/index.tsx
+++ src/components/specific/marketplace/datasetCards/verticalCollectionCard/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Avatar from 'boring-avatars';
 
 import DatasetTypeCard from '../../datasetTypeCard';
 import ProfileCard from '../../profileCard';
@@ -6,53 +7,56 @@ import ProfileCard from '../../profileCard';
 import classes from './styles.module.css';
 
 export default function VerticalCollectionCard(props: HomepageCollection) {
+
+   let gradientFunction = ()=>{
+    let gradients = ['#92A1C6', '#146A7C'];
+    return gradients[Math.round(Math.random()*1)];
+   }
+   let gradientFunction2 = ()=>{
+    let gradients = [ '#F0AB3D', '#C271B4', '#C20D90'];
+    return gradients[Math.round(Math.random()*2)];
+   }
     return (
         <div className={ classes.container }>
-            <Image
-                fill={ true }
-                src={ props.thumbnail?.url ?? '' }
+            <img
+                // fill={ true }
+                // src={ props.thumbnail?.url ?? '' }
                 alt="Thumbnail"
                 className={ classes.thumbnail }
+                style={{background: "linear-gradient(" + Math.round(Math.random()*360) +"deg, " + gradientFunction() + " " + Math.round(Math.random()*30) + "%," + gradientFunction2() + " " + Math.round(Math.random()*35 +70)  + "%)"}}
             />
+            {/* <Avatar
+            // className={ classes.thumbnail }
+            name={Math.random().toString()}
+            variant="marble"
+                colors={ ['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90'] }
+            /> */}
 
             <div className={ classes.contentContainer }>
                 <div className={ classes.header }>
-                    <img src={ props.icon?.url } alt="Avatar" />
+                    <img
+                        src="https://images.unsplash.com/photo-1528183429752-a97d0bf99b5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dHJlZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                        alt="Avatar"
+                    />
 
-                    <div className={ classes.contentContainer }>
-                        <div className={ classes.header }>
-                            <img
-                                src="https://images.unsplash.com/photo-1528183429752-a97d0bf99b5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dHJlZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                                alt="Avatar"
-                            />
+                    <DatasetTypeCard type={ props.type } className={classes.typeCard} />
+                </div>
 
-                            <DatasetTypeCard
-                                type={ props.type }
-                                className={ classes.typeCard }
-                            />
-                        </div>
+                <div className={ classes.middleSection }>
+                    <h1>{ props.name }</h1>
 
-                        <div className={ classes.middleSection }>
-                            <h1>{ props.name }</h1>
+                    <div className={ classes.profileCardContainer }>
+                        <ProfileCard
+                            className={ classes.profileCard }
+                            username={ props.user.username }
+                            profilePicture={ props.user.profilePhoto?.url ?? '' }
+                        />
+                    </div>
 
-                            <div className={ classes.profileCardContainer }>
-                                <ProfileCard
-                                    className={ classes.profileCard }
-                                    username={ props.user.username }
-                                    profilePicture={
-                                        props.user.profilePhoto?.url ?? ''
-                                    }
-                                />
-                            </div>
-                        </div>
-
-                        <div className={ classes.footer }>
-                            <div className={ classes.infoContainer }>
-                                <p className={ classes.infoHeader }>Datasets</p>
-                                <p className={ classes.info }>
-                                    { props.itemCount }
-                                </p>
-                            </div>
+                    <div className={ classes.footer }>
+                        <div className={ classes.infoContainer }>
+                            <p className={ classes.infoHeader }>Datasets</p>
+                            <p className={ classes.info }>{ props.itemCount }</p>
                         </div>
                     </div>
                 </div>
