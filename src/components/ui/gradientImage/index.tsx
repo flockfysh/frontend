ad0e133014f01b6344acedec7ecee1c387b2ebb6
AnshKetchum <eye.am.ansh@gmail.com>
2023-07-02 12:46:34 -0700

    Forgot to lint, whoops

--- src/components/ui/gradientImage/index.tsx
+++ src/components/ui/gradientImage/index.tsx
@@ -27,23 +27,23 @@ const GradientImage = (props) => {
 
   return (
     <svg
-      viewBox={'0 0 ' + SIZE + ' ' + SIZE}
+      viewBox={ '0 0 ' + SIZE + ' ' + SIZE }
       fill="none"
       role="img"
       xmlns="http://www.w3.org/2000/svg"
-      width={props.width}
-      height={props.height}
+      width={ props.width }
+      height={ props.height }
     >
-      {props.title && <title>{props.name}</title>}
-      <mask id={maskID} maskUnits="userSpaceOnUse" x={0} y={0} width={SIZE} height={SIZE}>
-        <rect width={SIZE} height={SIZE} rx={props.square ? undefined : SIZE * 2} fill="#FFFFFF" />
+      { props.title && <title>{ props.name }</title> }
+      <mask id={ maskID } maskUnits="userSpaceOnUse" x={ 0 } y={ 0 } width={ SIZE } height={ SIZE }>
+        <rect width={ SIZE } height={ SIZE } rx={ props.square ? undefined : SIZE * 2 } fill="#FFFFFF" />
       </mask>
-      <g mask={`url(#${maskID})`}>
-        <rect width={SIZE} height={SIZE} fill={properties[0].color} />
+      <g mask={ `url(#${maskID})` }>
+        <rect width={ SIZE } height={ SIZE } fill={ properties[0].color } />
         <path
           filter="url(#prefix__filter0_f)"
           d="M32.414 59.35L50.376 70.5H72.5v-71H33.728L26.5 13.381l19.057 27.08L32.414 59.35z"
-          fill={properties[1].color}
+          fill={ properties[1].color }
           transform={
             'translate(' +
             properties[1].translateX +
@@ -62,11 +62,11 @@ const GradientImage = (props) => {
         />
         <path
           filter="url(#prefix__filter0_f)"
-          style={{
+          style={ {
             mixBlendMode: 'overlay',
-          }}
+          } }
           d="M22.216 24L0 46.75l14.108 38.129L78 86l-3.081-59.276-22.378 4.005 12.972 20.186-23.35 27.395L22.215 24z"
-          fill={properties[2].color}
+          fill={ properties[2].color }
           transform={
             'translate(' +
             properties[2].translateX +
@@ -90,9 +90,9 @@ const GradientImage = (props) => {
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
         >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feFlood floodOpacity={ 0 } result="BackgroundImageFix" />
           <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur stdDeviation={7} result="effect1_foregroundBlur" />
+          <feGaussianBlur stdDeviation={ 7 } result="effect1_foregroundBlur" />
         </filter>
       </defs>
     </svg>
--- src/components/ui/gradientImage/utilities.tsx
+++ src/components/ui/gradientImage/utilities.tsx
@@ -1,40 +1,41 @@
 export const hashCode = (name) => {
-    var hash = 0;
-    for (var i = 0; i < name.length; i++) {
-        var character = name.charCodeAt(i);
+    let hash = 0;
+    for (let i = 0; i < name.length; i++) {
+        const character = name.charCodeAt(i);
         hash = ((hash<<5)-hash)+character;
         hash = hash & hash; // Convert to 32bit integer
     }
     return Math.abs(hash);
-  }
+  };
   
   export const getModulus = (num, max) => {
     return num % max;
-  }
+  };
   
   export const getDigit = (number, ntn) => {
     return Math.floor(( number/ Math.pow(10, ntn)) % 10);
-  }
+  };
   
   export const getBoolean = (number, ntn) => {
-    return (!((getDigit(number, ntn)) % 2))
-  }
+    return (!((getDigit(number, ntn)) % 2));
+  };
   
   export const getAngle = (x, y) => {
     return Math.atan2(y, x) * 180 / Math.PI;
-  }
+  };
   
   export const getUnit = (number, range, index) => {
-    let value = number % range
+    const value = number % range;
   
     if(index && ((getDigit(number, index) % 2) === 0)) {
-      return -value
-    } else return value
-  }
+      return -value;
+    }
+ else return value;
+  };
   
   export const getRandomColor = (number, colors, range) => {
-    return colors[(number) % range]
-  }
+    return colors[(number) % range];
+  };
   
   export const getContrast = (hexcolor) => {
   
@@ -44,12 +45,12 @@ export const hashCode = (name) => {
       }
   
       // Convert to RGB value
-      var r = parseInt(hexcolor.substr(0,2),16);
-      var g = parseInt(hexcolor.substr(2,2),16);
-      var b = parseInt(hexcolor.substr(4,2),16);
+      const r = parseInt(hexcolor.substr(0, 2), 16);
+      const g = parseInt(hexcolor.substr(2, 2), 16);
+      const b = parseInt(hexcolor.substr(4, 2), 16);
   
       // Get YIQ ratio
-      var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+      const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
   
       // Check contrast
       return (yiq >= 128) ? '#000000' : '#FFFFFF';
