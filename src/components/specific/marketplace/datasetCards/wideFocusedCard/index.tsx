25b736c021de64accf7bdd19df8272d438eb117d
root <root@SHIVA.localdomain>
2023-07-15 10:34:32 +0530

    Changes for milestone 1

--- src/components/specific/marketplace/datasetCards/wideFocusedCard/index.tsx
+++ src/components/specific/marketplace/datasetCards/wideFocusedCard/index.tsx
@@ -1,91 +1,89 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 import DatasetTypeCard from '../../datasetTypeCard';
 
 import { formatFileSize } from '@/helpers/formatting';
-import { getDefaultDatasetThumbnail } from '@/helpers/defaults';
 
 import classes from './styles.module.css';
 
 export default function WideFocusedCard(props: HomepageDataset) {
-    const gradientFunction = () => {
-        const gradients = ['#92A1C6', '#146A7C'];
-        return gradients[Math.round(Math.random() * 1)];
-    };
+  const gradientFunction = () => {
+    const gradients = ['#92A1C6', '#146A7C'];
+    return gradients[Math.round(Math.random() * 1)];
+  };
 
-    const gradientFunction2 = () => {
-        const gradients = ['#F0AB3D', '#C271B4', '#C20D90'];
-        return gradients[Math.round(Math.random() * 2)];
-    };
-    
-    return (
-        <div className={ classes.cardContainer }>
-            <div className={ classes.header }>
-                <div className={ classes.thumbnail }>
-                    { props.icon && <img src={ props.icon?.url } alt="avatar"/> }
-                </div>
+  const gradientFunction2 = () => {
+    const gradients = ['#F0AB3D', '#C271B4', '#C20D90'];
+    return gradients[Math.round(Math.random() * 2)];
+  };
 
-                <DatasetTypeCard
-                    className={ classes.typeContainer }
-                    type={ props.type }
-                />
-            </div>
-
-            <div
-                className={ classes.image }
-                style={ {
-                    background:
-                        'linear-gradient(' +
-                        Math.round(Math.random() * 360) +
-                        'deg, ' +
-                        gradientFunction() +
-                        ' ' +
-                        Math.round(Math.random() * 30) +
-                        '%,' +
-                        gradientFunction2() +
-                        ' ' +
-                        Math.round(Math.random() * 35 + 70) +
-                        '%)',
-                } }
-            />
-
-            <div className={ classes.overlay } />
-            
-            <Link
-                className={ classes.linkOverlay }
-                href={ `/marketplace/${props._id}` }
-            />
-            
-            <div className={ classes.datasetName }>{ props.name }</div>
-
-            <div className={ classes.footer }>
-                <div className={ classes.left }>
-                    <img src={ props.user.profilePhoto?.url } alt="pfp"/>
-
-                    <p>@{ props.user.username.slice(0, 16) }</p>
-                </div>
-
-                <div className={ classes.right }>
-                    <div className={ classes.infoContainer }>
-                        <span className={ classes.infoContainerLabel }>
-                            Items
-                        </span>
-
-                        <span className={ classes.infoContainerValue }>
-                            { props.assetCounts.total }
-                        </span>
-                    </div>
-
-                    <div className={ classes.infoContainer }>
-                        <span className={ classes.infoContainerLabel }>Size</span>
-
-                        <span className={ classes.infoContainerValue }>
-                            { formatFileSize(props.size.total.total) }
-                        </span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={classes.cardContainer}>
+      <div className={classes.header}>
+        <div className={classes.thumbnail}>
+          {props.icon && <img src={props.icon?.url} alt="avatar" />}
         </div>
-    );
+
+        <DatasetTypeCard className={classes.typeContainer} type={props.type} />
+      </div>
+
+      <div
+        className={classes.image}
+        style={{
+          background:
+            'linear-gradient(' +
+            Math.round(Math.random() * 360) +
+            'deg, ' +
+            gradientFunction() +
+            ' ' +
+            Math.round(Math.random() * 30) +
+            '%,' +
+            gradientFunction2() +
+            ' ' +
+            Math.round(Math.random() * 35 + 70) +
+            '%)',
+        }}
+      />
+
+      <div className={classes.overlay} />
+
+      <Link
+        className={classes.linkOverlay}
+        href={`/marketplace/${props._id}`}
+      />
+
+      <div className={classes.datasetName}>{props.name}</div>
+
+      <div className={classes.footer}>
+        <div className={classes.left}>
+          <Link
+            className={classes.anchor}
+            href={`/profile/${props.user.username}`}
+          >
+            <img src={props.user.profilePhoto?.url} alt="pfp" />
+
+            <p>@{props.user.username.slice(0, 16)}</p>
+          </Link>
+        </div>
+
+        <div className={classes.right}>
+          <div className={classes.infoContainer}>
+            <span className={classes.infoContainerLabel}>Items</span>
+
+            <span className={classes.infoContainerValue}>
+              {props.assetCounts.total}
+            </span>
+          </div>
+
+          <div className={classes.infoContainer}>
+            <span className={classes.infoContainerLabel}>Size</span>
+
+            <span className={classes.infoContainerValue}>
+              {formatFileSize(props.size.total.total)}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/specific/marketplace/navbar/index.tsx
+++ src/components/specific/marketplace/navbar/index.tsx
@@ -1,95 +1,184 @@
-import { useState, useContext } from 'react';
-import { ReactSVG } from 'react-svg';
-import Link from 'next/link';
+import CurrentUserProfile from "@/components/specific/marketplace/currentUserProfile";
+import searchIcon from "@/icons/main/search.svg";
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import { ReactSVG } from "react-svg";
+import Login from "../../login";
+import CreateDatasetModal from "../createDatasetModal";
+import CreatePostModal from "../createPostModal";
 
-import CurrentUserProfile from '@/components/specific/marketplace/currentUserProfile';
-import CreateDatasetModal from '../createDatasetModal';
-import CreatePostModal from '../createPostModal';
-import Login from '../../login';
+import { UserContext } from "@/contexts/userContext";
 
-import { UserContext } from '@/contexts/userContext';
+import fish from "@/icons/branding/fish.svg";
+import bell from "@/icons/main/bell.svg";
+import penTool from "@/icons/main/pen-tool.svg";
+import plusCircle from "@/icons/main/plus-circle.svg";
+import userIcon from "@/icons/main/user.svg";
 
-import fish from '@/icons/branding/fish.svg';
-import search from '@/icons/main/search.svg';
-import bell from '@/icons/main/bell.svg';
-import plusCircle from '@/icons/main/plus-circle.svg';
-import penTool from '@/icons/main/pen-tool.svg';
-import userIcon from '@/icons/main/user.svg';
-
-import classes from './styles.module.css';
-import { ModalContext } from '@/contexts/modalContext';
+import { ModalContext } from "@/contexts/modalContext";
+import api from "@/helpers/api";
+import classes from "./styles.module.css";
 
 export default function MarketplaceNavbar() {
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
+  const [searchedUser, setSearchedUser] = useState<RedactedUser[]>([]);
+  const [searchedDataset, setsearchedDataset] = useState<HomepageDataset[]>([]);
 
-    const [isLogin, updateLogin] = useState(false);
-    const [isDatasetModalOpen, updateDatasetModalOpen] = useState(false);
-    const { isCreatePostOpen, setCreatePostOpen } = useContext(ModalContext);
+  const [isLogin, updateLogin] = useState(false);
+  const [isDatasetModalOpen, updateDatasetModalOpen] = useState(false);
+  const { isCreatePostOpen, setCreatePostOpen } = useContext(ModalContext);
 
-    return (
-        <nav className={ classes.nav }>
-            { isDatasetModalOpen && (
-                <CreateDatasetModal onClose={ () => updateDatasetModalOpen(false) } />
-            ) }
-            
-            { isCreatePostOpen && (
-                <CreatePostModal onClose={ () => setCreatePostOpen(false) } />
-            ) }
+  useEffect(() => {
+    const fetchTimerId = setTimeout(async () => {
+      if (search.length > 0) {
+        const fetchedDataset = (
+          await api.get<Api.PaginatedResponse<HomepageDataset[]>>(
+            "/api/datasets/search",
+            {
+              params: {
+                name: search,
+              },
+            }
+          )
+        ).data;
 
-            { isLogin && (
-                <Login mode="login" onClose={ () => updateLogin(false) } />
-            ) }
+        const fetchedUsers = (
+          await api.get<Api.PaginatedResponse<RedactedUser[]>>(
+            "/api/users/search",
+            {
+              params: {
+                query: search,
+              },
+            }
+          )
+        ).data;
 
-            <div className={ classes.subContainer + ' ' + classes.leftContainer }>
-                <Link className={ classes.logoContainer } href="/marketplace">
-                    <ReactSVG src={ fish.src } />
+        setsearchedDataset(fetchedDataset.data);
+        setSearchedUser(fetchedUsers.data);
 
-                    <p>fDE</p>
-                </Link>
+        setOpen(true);
+      } else {
+        setOpen(false);
+      }
+    }, 2000);
 
-                <label className={ classes.searchContainer }>
-                    <ReactSVG src={ search.src } className={ classes.searchIcon } />
+    return () => clearTimeout(fetchTimerId);
+  }, [search]);
 
-                    <input
-                        type="search"
-                        className={ classes.search }
-                        placeholder="Search by username or dataset name"
-                    />
-                </label>
+  console.log(search, open);
 
-                <ReactSVG src={ search.src } className={ classes.mobileSearch } />
-            </div>
+  return (
+    <nav className={classes.nav}>
+      {isDatasetModalOpen && (
+        <CreateDatasetModal onClose={() => updateDatasetModalOpen(false)} />
+      )}
 
-            <div className={ classes.subContainer }>
-                { user ? (
-                    <>
-                        <ReactSVG
-                            onClick={ () => setCreatePostOpen(true) }
-                            src={ penTool.src }
-                            className={ classes.leftIcon }
-                        />
-                        <ReactSVG
-                            onClick={ () => updateDatasetModalOpen(true) }
-                            src={ plusCircle.src }
-                            className={ classes.leftIcon }
-                        />
-                        <ReactSVG src={ bell.src } className={ classes.leftIcon } />
+      {isCreatePostOpen && (
+        <CreatePostModal onClose={() => setCreatePostOpen(false)} />
+      )}
 
-                        <CurrentUserProfile showMenu={ true } />
-                    </>
-                ) : (
-                    <button
-                        className={ classes.loginButton }
-                        onClick={ () => updateLogin(true) }
+      {isLogin && <Login mode='login' onClose={() => updateLogin(false)} />}
+
+      <div className={classes.subContainer + " " + classes.leftContainer}>
+        <Link className={classes.logoContainer} href='/marketplace'>
+          <ReactSVG src={fish.src} />
+
+          <p>fDE</p>
+        </Link>
+
+        <label
+          className={classes.searchContainer}
+          onBlur={() => {
+            setOpen(false);
+          }}
+          onFocus={() => {
+            setOpen(true);
+          }}
+        >
+          <ReactSVG src={searchIcon.src} className={classes.searchIcon} />
+
+          <input
+            type='search'
+            className={classes.search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            placeholder='Search by username or dataset name'
+          />
+          <div
+            id='menu-div'
+            className={`${classes.searchResult} ${
+              open ? classes.searchResultActive : ""
+            }`}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {searchedUser.length > 0 && (
+              <>
+                <h2 className={classes.searchResultHeader}>Users</h2>
+                {searchedUser.slice(0, 3).map((user, userIndex) => {
+                  return (
+                    <Link
+                      href={user.link}
+                      key={userIndex}
+                      className={classes.searchResultItem}
                     >
-                        Login{ ' ' }
-                        <ReactSVG
-                            className={ classes.loginIcon }
-                            src={ userIcon.src }
-                        />
-                    </button>
-                ) }
-            </div>
-        </nav>
-    );
+                      {user.name}
+                    </Link>
+                  );
+                })}
+              </>
+            )}
+
+            {searchedDataset.length > 0 && (
+              <>
+                <h2 className={classes.searchResultHeader}>Datasets</h2>
+                {searchedDataset.slice(0, 3).map((dataset, datasetIndex) => {
+                  return (
+                    <Link
+                      href={dataset.link}
+                      key={datasetIndex}
+                      className={classes.searchResultItem}
+                    >
+                      {dataset.name}
+                    </Link>
+                  );
+                })}
+              </>
+            )}
+          </div>
+        </label>
+
+        <ReactSVG src={searchIcon.src} className={classes.mobileSearch} />
+      </div>
+
+      <div className={classes.subContainer}>
+        {user ? (
+          <>
+            <ReactSVG
+              onClick={() => setCreatePostOpen(true)}
+              src={penTool.src}
+              className={classes.leftIcon}
+            />
+            <ReactSVG
+              onClick={() => updateDatasetModalOpen(true)}
+              src={plusCircle.src}
+              className={classes.leftIcon}
+            />
+            <ReactSVG src={bell.src} className={classes.leftIcon} />
+
+            <CurrentUserProfile showMenu={true} />
+          </>
+        ) : (
+          <button
+            className={classes.loginButton}
+            onClick={() => updateLogin(true)}
+          >
+            Login <ReactSVG className={classes.loginIcon} src={userIcon.src} />
+          </button>
+        )}
+      </div>
+    </nav>
+  );
 }
