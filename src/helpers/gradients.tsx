f2335c398b9fb329bce59b81267d77c5593270ed
AnshKetchum <eye.am.ansh@gmail.com>
2023-08-30 19:38:11 -0700

    Patch divs

--- src/helpers/gradients.tsx
+++ src/helpers/gradients.tsx
@@ -31,12 +31,24 @@ export function RandomGradientComponent({ className } : {className: string}) {
   const gradientStyle = generateGradientStyle(gradientColors);
 
   return (
-    <div>
-        <div style={ { objectFit: 'cover', backgroundImage: gradientStyle } } className = { className }>
+    <div
+        style={ {
+            position: 'relative',
+            width: '100%',
+            height: '100vh',
+            backgroundImage: gradientStyle,
+        } }
+
+        className= { className }
+
+    >
         
-        
-        </div>
-        
+        <Image
+            src="https://images.unsplash.com/photo-1579818277076-1abc45c9471f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80"
+            fill={ true }
+            style={ { backgroundImage: gradientStyle } }
+            alt = "#"
+        />
     </div>
   );
     
