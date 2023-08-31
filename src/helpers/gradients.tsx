9de211124ab84bd4406e839229f2b36a92c8b4a6
AnshKetchum <eye.am.ansh@gmail.com>
2023-08-30 19:39:49 -0700

    No image

--- src/helpers/gradients.tsx
+++ src/helpers/gradients.tsx
@@ -44,7 +44,7 @@ export function RandomGradientComponent({ className } : {className: string}) {
     >
         
         <Image
-            src="https://images.unsplash.com/photo-1579818277076-1abc45c9471f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80"
+            src=""
             fill={ true }
             style={ { backgroundImage: gradientStyle } }
             alt = "#"
