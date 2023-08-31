654436724cae20c772f2d36cb049cf9ecac0a730
AnshKetchum <eye.am.ansh@gmail.com>
2023-08-30 19:13:39 -0700

    Update

--- src/helpers/gradients.tsx
+++ src/helpers/gradients.tsx
@@ -19,7 +19,7 @@ const generateGradientStyle = (colors) => {
   return `linear-gradient(to bottom right, ${colors[0]}, ${colors[1]})`;
 };
 
-export function RandomGradientComponent() {
+export function RandomGradientComponent({ className } : {className: string}) {
   const [gradientColors, setGradientColors] = useState([]);
 
   useEffect(() => {
@@ -36,6 +36,7 @@ export function RandomGradientComponent() {
             src="https://images.unsplash.com/photo-1579818277076-1abc45c9471f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80"
             layout="fill"
             objectFit="cover"
+            className= { className }
             style={ { backgroundImage: gradientStyle } }
         />
     </div>
