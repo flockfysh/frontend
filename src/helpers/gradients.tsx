577d968c7988c9f4bb16b91fa5884b13360a8bf9
AnshKetchum <eye.am.ansh@gmail.com>
2023-08-30 20:09:26 -0700

    Improve gradient algorithm

--- src/helpers/gradients.tsx
+++ src/helpers/gradients.tsx
@@ -15,20 +15,31 @@ const generateColorPalette = (baseColor:any) => {
   return [baseColor, complementaryColor];
 };
 
-const generateGradientStyle = (colors:any) => {
-  return `linear-gradient(to bottom right, ${colors[0]}, ${colors[1]})`;
-};
+const generateVibrantColor = (): string => {
+    const h = Math.random() * 360;
+    const s = 70 + Math.random() * 20; // Adjust saturation for vibrancy
+    const l = 50 + Math.random() * 20; // Adjust lightness for vibrancy
+    return `hsl(${h},${s}%,${l}%)`;
+  };
+  
+  const generateGradientStyle = (colors: string[], angle: number): string => {
+    return `linear-gradient(${angle}deg, ${colors[0]}, ${colors[1]})`;
+  };
 
 export function RandomGradientComponent({ className } : {className: string}) {
   const [gradientColors, setGradientColors] = useState([]);
+  const [gradientAngle, setGradientAngle] = useState<number>(0);
 
   useEffect(() => {
-    const baseColor = generateRandomColor();
-    const paletteColors:any = generateColorPalette(baseColor);
+    const baseColor = generateVibrantColor();
+    const paletteColors:any = [baseColor, generateVibrantColor()];
     setGradientColors(paletteColors);
+
+    const randomAngle = Math.floor(Math.random() * 360);
+    setGradientAngle(randomAngle);
   }, []);
 
-  const gradientStyle = generateGradientStyle(gradientColors);
+  const gradientStyle = generateGradientStyle(gradientColors, gradientAngle);
 
   return (
     <div
