5800b3aa1b0c7423488910ce79723639797c47bf
AnshKetchum <eye.am.ansh@gmail.com>
2023-09-14 14:15:10 -0700

    Remove async

--- src/components/ui/loadingIcon/index.tsx
+++ src/components/ui/loadingIcon/index.tsx
@@ -3,10 +3,9 @@
 import loadingIcon from '@/icons/loading.svg';
 import classes from './loadingIcon.module.css';
 
-//React.ComponentPropsWithRef<'img'>
-export default function LoadingIcon() {
+export default function LoadingIcon(props: React.ComponentPropsWithRef<'img'>) {
     return (
-        <img src={ loadingIcon } alt={ 'Loading...' } 
-             className={ `${classes.loadingIcon}` }/>
+        <img src={ loadingIcon } alt={ 'Loading...' } { ...props }
+             className={ `${classes.loadingIcon} ${props.className || ''}` }/>
     );
 }
