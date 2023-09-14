beba2193db727db74f9e9fd7214229d1eed9ad0d
AnshKetchum <eye.am.ansh@gmail.com>
2023-09-14 14:05:38 -0700

    Make props optional

--- src/components/ui/loadingIcon/index.tsx
+++ src/components/ui/loadingIcon/index.tsx
@@ -3,7 +3,7 @@
 import loadingIcon from '@/icons/loading.svg';
 import classes from './loadingIcon.module.css';
 
-export default function LoadingIcon(props: React.ComponentPropsWithRef<'img'>) {
+export default function LoadingIcon(props?: React.ComponentPropsWithRef<'img'>) {
     return (
         <img src={ loadingIcon } alt={ 'Loading...' } { ...props }
              className={ `${classes.loadingIcon} ${props.className || ''}` }/>
