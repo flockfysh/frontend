85cf8c9dcdc3d8652e1a923a7c4ec996dc06e88c
AnshKetchum <eye.am.ansh@gmail.com>
2023-09-14 14:20:05 -0700

    Debug loading

--- src/components/ui/loadingIcon/index.tsx
+++ src/components/ui/loadingIcon/index.tsx
@@ -1,9 +1,10 @@
 // // TODO: Fix this component
 
-import loadingIcon from '@/icons/loading.svg';
+import loading from '@/icons/loading.svg';
 import classes from './loadingIcon.module.css';
 
 export default function LoadingIcon(props: React.ComponentPropsWithRef<'img'>) {
+    console.log(loading);
     return (
         <img src={ loadingIcon } alt={ 'Loading...' } { ...props }
              className={ `${classes.loadingIcon} ${props.className || ''}` }/>
