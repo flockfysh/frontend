5335dd68459ddc089b7f46769aa2ca7a8eb2734a
AnshKetchum <eye.am.ansh@gmail.com>
2023-09-14 14:10:16 -0700

    Remove props entirely

--- src/components/ui/loadingIcon/index.tsx
+++ src/components/ui/loadingIcon/index.tsx
@@ -4,9 +4,9 @@ import loadingIcon from '@/icons/loading.svg';
 import classes from './loadingIcon.module.css';
 
 //React.ComponentPropsWithRef<'img'>
-export default function LoadingIcon(props: any) {
+export default function LoadingIcon() {
     return (
-        <img src={ loadingIcon } alt={ 'Loading...' } { ...props }
-             className={ `${classes.loadingIcon} ${props.className || ''}` }/>
+        <img src={ loadingIcon } alt={ 'Loading...' } 
+             className={ `${classes.loadingIcon}` }/>
     );
 }
