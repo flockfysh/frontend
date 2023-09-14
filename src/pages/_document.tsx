25947eb9878c5fd083d16c7dc0925867d7c0b74c
Ansh <eye.am.ansh@gmail.com>
2023-09-14 10:10:24 -0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta
           name="description"
-          content="lightning fast large scalle ML datasets"
+          content="lightning fast, large scale ML datasets"
         />
 
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
@@ -14,6 +14,7 @@ export default function Document() {
           property="og:title"
           content="flockfysh | lightning fast large scale ML datasets"
         />
+        
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://www.flockfysh.ai/" />
         <meta property="og:image" content="" />
@@ -21,7 +22,10 @@ export default function Document() {
           property="og:description"
           content="lightning fast large scale ML datasets"
         />
-
+        <meta 
+          name="google-adsense-account" 
+          content="ca-pub-8241648335060628"
+          />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
