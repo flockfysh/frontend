48a95bfff5e565b1f22e86a269219b7a6a16c7b3
AnshKetchum <eye.am.ansh@gmail.com>
2023-09-14 14:25:20 -0700

    change hype page layout

--- src/components/ui/loadingIcon/index.tsx
+++ src/components/ui/loadingIcon/index.tsx
@@ -1,10 +1,9 @@
 // // TODO: Fix this component
 
-import loading from '@/icons/loading.svg';
+import loading from '@/icons/branding/loading.svg';
 import classes from './loadingIcon.module.css';
 
 export default function LoadingIcon(props: React.ComponentPropsWithRef<'img'>) {
-    console.log(loading);
     return (
         <img src={ loadingIcon } alt={ 'Loading...' } { ...props }
              className={ `${classes.loadingIcon} ${props.className || ''}` }/>
